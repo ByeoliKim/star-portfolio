d0eb38a14010fd6e75570bf18293e6d4f4d29e09
ByeoliKim <213069@naver.com>
2025-12-08 23:02:26 +0900

    feat: 페이지 라우팅 세팅

---
+++ app/card/page.tsx
@@ -0,0 +1,7 @@
+"use cient";
+
+export default function Card() {
+  <>
+    <div className="min-h-screen relative mt-60 mx-auto">명함 저장 페이지</div>
+  </>;
+}
---
+++ app/mbti/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { CompanyMatchTest } from "@/components/mbti/CompanyMatchTest";
+
+export default function Mbti() {
+  return (
+    <>
+      <div className="min-h-[70vh] relative mt-16 mx-auto">
+        <CompanyMatchTest />
+      </div>
+    </>
+  );
+}
